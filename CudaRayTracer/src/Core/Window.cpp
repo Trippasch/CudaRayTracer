c3479c8fb4c541067eed101577eeb6fef77f3745
Paschalis Choropanitis <pasxoshorp13@gmail.com>
2022-07-12 21:11:11 +0300

    Added ImGui docking

--- CudaRayTracer/src/Core/Window.cpp
+++ CudaRayTracer/src/Core/Window.cpp
@@ -189,16 +189,70 @@ void Window::OnUpdate()
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    /***************** ImGui *****************/
+    /*****************ImGui *****************/
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
-
     ImGui::NewFrame();
-    {
-        ImGui::ShowDemoWindow();
-        ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
 
-        ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+    {
+        static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+        // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+        // because it would be confusing to have two docking targets within each others.
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDocking;
+        // if (m_MenubarCallback)
+        //     window_flags |= ImGuiWindowFlags_MenuBar;
+
+        const ImGuiViewport *viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->WorkPos);
+        ImGui::SetNextWindowSize(viewport->WorkSize);
+        ImGui::SetNextWindowViewport(viewport->ID);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+        window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+
+        // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+        // and handle the pass-thru hole, so we ask Begin() to not render a background.
+        if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+            window_flags |= ImGuiWindowFlags_NoBackground;
+
+        // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+        // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+        // all active windows docked into it will lose their parent and become undocked.
+        // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+        // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("DockSpace Demo", nullptr, window_flags);
+        ImGui::PopStyleVar();
+
+        ImGui::PopStyleVar(2);
+
+        // Submit the DockSpace
+        ImGuiIO &io = ImGui::GetIO();
+        if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+        {
+            ImGuiID dockspace_id = ImGui::GetID("VulkanAppDockspace");
+            ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+        }
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Begin("Hello");
+        ImGui::End();
+
+        // if (m_MenubarCallback)
+        // {
+        //     if (ImGui::BeginMenuBar())
+        //     {
+        //         m_MenubarCallback();
+        //         ImGui::EndMenuBar();
+        //     }
+        // }
+
+        // for (auto &layer : m_LayerStack)
+        //     layer->OnUIRender();
+
         ImGui::End();
     }
 
@@ -213,7 +267,7 @@ void Window::OnUpdate()
         glfwMakeContextCurrent(backup_current_context);
     }
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-    /******************************************/
+    /****************************************/
 
     /* Swap front and back buffers */
     glfwSwapBuffers(m_Window);
