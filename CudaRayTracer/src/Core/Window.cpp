ae63795dc2e23929bd65fada83240eb0197d1d18
Paschalis Choropanitis <pasxoshorp13@gmail.com>
2022-07-14 22:15:47 +0300

    Updated ImGui Project

--- CudaRayTracer/src/Core/Window.cpp
+++ CudaRayTracer/src/Core/Window.cpp
@@ -232,7 +232,7 @@ void Window::OnUpdate()
         ImGuiIO &io = ImGui::GetIO();
         if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
         {
-            ImGuiID dockspace_id = ImGui::GetID("VulkanAppDockspace");
+            ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
             ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
         }
 
