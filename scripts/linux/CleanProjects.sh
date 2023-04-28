0f20ddd16f5406cc714df1cdd767a44787e7f531
Trippasch <pasxoshorp13@gmail.com>
2023-04-28 18:45:05 +0300

    Fixed CleanProjects.sh

--- scripts/linux/CleanProjects.sh
+++ scripts/linux/CleanProjects.sh
@@ -6,7 +6,7 @@ echo "find . -name 'Makefile' -delete"
 find ./ -name 'Makefile' -delete
 
 echo "rm -rf ./bin ./vendor/Glad/bin ./vendor/GLFW/bin ./vendor/ImGui/bin"
-rm -rf ./bin ./../../vendor/Glad/bin ./../../vendor/GLFW/bin ./../../vendor/ImGui/bin 
+rm -rf ./bin ./vendor/Glad/bin ./vendor/GLFW/bin ./vendor/ImGui/bin 
 
 echo "rm -rf ./bin-int ./vendor/Glad/bin-int ./vendor/GLFW/bin-int ./vendor/ImGui/bin-int"
 rm -rf ./bin-int ./vendor/Glad/bin-int ./vendor/GLFW/bin-int ./vendor/ImGui/bin-int 
